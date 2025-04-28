6cc3ba776e926c8b44cd6c546abbc4e23e9e7f06
mishalturkane <mishalturkane@gmail.com>
2025-04-28 14:14:01 +0000

    escrow code updated

--- escrow-anchor/programs/escrow-anchor/src/lib.rs
+++ escrow-anchor/programs/escrow-anchor/src/lib.rs
@@ -1,13 +1,13 @@
-pub mod constants;
-pub mod error;
+
 pub mod instructions;
 pub mod state;
 
+
 use anchor_lang::prelude::*;
 
-pub use constants::*;
 pub use instructions::*;
 pub use state::*;
+pub use make::*;
 
 declare_id!("3LWpKUZnH3HAG9KUrYbVqqJPmN9DmKv8KypsnUDVf4ht");
 
@@ -15,7 +15,7 @@ declare_id!("3LWpKUZnH3HAG9KUrYbVqqJPmN9DmKv8KypsnUDVf4ht");
 pub mod escrow_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make(ctx: Context<Make>,seed: u64, deposit: u64, recieve: u64) -> Result<()> {
+            ctx.accounts.deposit(deposit)
     }
 }
