39d7ed7121731cf763d37f4d536a856daa6813bf
mishalturkane <mishalturkane@gmail.com>
2025-05-14 18:52:57 +0000

    code updated

--- snap-ledger/programs/snap-ledger/src/instructions/pay_bill.rs
+++ snap-ledger/programs/snap-ledger/src/instructions/pay_bill.rs
@@ -1,5 +1,4 @@
 use anchor_lang::prelude::*;
-use std::time::{SystemTime, UNIX_EPOCH};
 use anchor_lang::solana_program::sysvar::clock::Clock;
 use crate::Receipt;
 use crate::Merchant;
@@ -31,21 +30,6 @@ impl<'info> PayBill<'info> {
         if !merchant.authority.eq(&receipt.merchant) {
             return Err(ErrorCode::UnauthorizedMerchant.into()); // Custom error
         }
-
-        // Validate the timestamp - 24 hours validity window
-        let clock = Clock::get()?;
-        let current_time = clock.unix_timestamp;
-    //  let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64;
-
-        let time_difference = current_time - receipt.timestamp;
-
-        const VALIDITY_PERIOD: i64 = 86400; // 24 hours in seconds (86400 seconds)
-
-        if time_difference > VALIDITY_PERIOD {
-            return Err(ErrorCode::ReceiptExpired.into()); // Custom error
-        }
-
-        // If everything is valid, proceed with the payment (you may want to implement payment logic here)
         Ok(())
     }
 }
